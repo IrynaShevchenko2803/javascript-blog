9cc1da4c290eedb22264e3679e9ffc98e0c35a93
Irina <irynashevchenkozzz@gmail.com>
2023-01-12 03:16:34 +0300

    add function remove active

--- js/script.js
+++ js/script.js
@@ -1,21 +1,36 @@
 'use strict';
 
+
 function titleClickHandler(event){
     console.log('Link was clicked!');
-  
-    /* remove class 'active' from all article links  */
-  
-    /* add class 'active' to the clicked link */
-  
-    /* remove class 'active' from all articles */
-  
-    /* get 'href' attribute from the clicked link */
-  
-    /* find the correct article using the selector (value of 'href' attribute) */
-  
-    /* add class 'active' to the correct article */
   }
-  
+   /* remove class 'active' from all article links  */
+
+   const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
+
+  /* add class 'active' to the clicked link */
+
+  /* remove class 'active' from all articles */
+
+  const activeArticles = document.querySelectorAll('article.post')
+
+  for ( let activeArticle of activeArticles){
+    activeArticle.classList.remove('active')
+  }
+
+
+
+  /* get 'href' attribute from the clicked link */
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+
+  /* add class 'active' to the correct article */
+
+
   const links = document.querySelectorAll('.titles a');
   
   for(let link of links){
