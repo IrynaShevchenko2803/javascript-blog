236ba901624d25de288ab97baedbd19359e686bf
Irina <irynashevchenkozzz@gmail.com>
2023-01-12 18:45:38 +0300

    add classes active and remove active

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@
 
 
 function titleClickHandler(event){
+  const clickedElement = this;
     console.log('Link was clicked!');
-  }
-   /* remove class 'active' from all article links  */
+  
+   /* [DONE]remove class 'active' from all article links  */
 
    const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -12,16 +13,22 @@ for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-  /* add class 'active' to the clicked link */
+  /*[IN PROGRESS] add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+  
+  clickedElement.classList.add('active')
+  console.log(clickedElement)
+
+
+
+  /* [DONE]remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('article.post')
 
   for ( let activeArticle of activeArticles){
     activeArticle.classList.remove('active')
   }
-
+}
 
 
   /* get 'href' attribute from the clicked link */
