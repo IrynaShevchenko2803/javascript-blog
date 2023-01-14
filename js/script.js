87fc24446a3165fd03aeb3bd502d3556ea829bde
Irina <irynashevchenkozzz@gmail.com>
2023-01-14 22:57:59 +0300

    the finish of 5 modules (to add two functions)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
 
 function titleClickHandler(event){
   event.preventDefault();
@@ -56,3 +60,56 @@ const list = document.querySelectorAll(".list a")
 for (const l of list ){
  l.addEventListener('click', titleClickHandler)
 }
+
+
+/* ---------------------------------------------THE SECOND FUNCTION------------------------------------------------------------- */
+/* [DONE] remove contents of titleList */
+
+function generateTitleLinks (customSelector=''){
+
+const titleList = document.querySelectorAll(optTitleListSelector)
+titleList.innerHTML = ''
+
+
+
+/*[DONE]потім для кожної статті:
+ /* for each article */
+
+const allArticles = document.querySelectorAll('.post' + customSelector);
+  let html = '';
+  for(let article of allArticles){
+
+/*-[DONE]прочитайте її id і збережіть його в константі
+get the article id*/
+
+  const articleId = article.getAttribute('id')
+
+/*-[DONE]знайдіть елемент із заголовком і збережіть його вміст у константі,
+find the title element*/
+
+  const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+/*-на основі цієї інформації створіть код HTML посилання та збережіть його в константі, 
+-вставте створений код HTML у список посилань у лівій колонці.*/
+ /* [DONE]create HTML of the link */
+
+ const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+ console.log(linkHTML)
+    /* insert link into titleList */
+    html = html + linkHTML;
+  }
+    titleList.innerHTML = html;
+
+  
+
+  const links = document.querySelectorAll('.titles a');
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
+}
+generateTitleLinks ()
+
+/*------------------------------------------------------------------------------------------- */
+
+
+
